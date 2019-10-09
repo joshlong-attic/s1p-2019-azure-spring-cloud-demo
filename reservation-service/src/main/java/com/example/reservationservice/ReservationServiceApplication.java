003e5f23bfef919a94f04ce1b9ea77fb8d94e516
Josh Long <josh@joshlong.com>
2019-10-08 21:31:00 -0500

    updated code..

--- reservation-service/src/main/java/com/example/reservationservice/ReservationServiceApplication.java
+++ reservation-service/src/main/java/com/example/reservationservice/ReservationServiceApplication.java
@@ -3,13 +3,19 @@ package com.example.reservationservice;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.event.EventListener;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Flux;
 
 import java.time.Instant;
 
@@ -29,6 +35,23 @@ public class ReservationServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ReservationServiceApplication.class, args);
+	}
+}
+
+@Component
+@RequiredArgsConstructor
+@Log4j2
+class Initializer {
+
+	private final ReservationRepository reservationRepository;
+
+	@EventListener(ApplicationReadyEvent.class)
+	public void ready() {
+		Flux
+			.just("Kylie", "Julien", "Theresa", "Josh", "Emily", "Bruno", "Julia", "Madhura")
+			.map(name -> new Reservation(null, name))
+			.flatMap(this.reservationRepository::save)
+			.subscribe(log::info);
 	}
 }
 
